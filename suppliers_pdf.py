7fa12aa644b9f27bed5a2d4655c1f851b5437174
Arina2707 <arina.odnoblyudova@mail.ru>
2021-01-17 13:45:43 +0300

    commit

--- suppliers_pdf.py
+++ suppliers_pdf.py
@@ -13,11 +13,11 @@ os.chdir(folder)
 
 # save pdf as jpg (if there are several pages --> img for each)
 jpgs_list, texts_list = [], []
-image = "PO17-10.pdf"
+image = "PO18-3.pdf"
 images = convert_from_path(image)
 for i, img in enumerate(images):
-    jpgs_list.append(image[:-4]+'_output'+str(i)+'.jpg')
-    img.save(image[:-4]+'_output'+str(i)+'.jpg', 'JPEG')
+    jpgs_list.append(image[:-4] + '_output' + str(i) + '.jpg')
+    img.save(image[:-4] + '_output' + str(i) + '.jpg', 'JPEG')
 
 # load image and convert to grayscale
 image = cv2.imread(jpgs_list[0])
@@ -35,12 +35,38 @@ text = pytesseract.image_to_string(Image.open(filename))
 os.remove(filename)
 texts_list.append(text)
 
-# processing texts
-text_items = text[(text.find('TOTAL PRICE')+11):]
-text_items = text_items[:(text_items.find('Total USD'))]
-print(text_items.splitlines())
-
 # show the output images
 # cv2.imshow("Image", image)
 # cv2.imshow("Output", gray)
 # cv2.waitKey(0)
+
+# processing texts
+text_items = text[(text.find('TOTAL PRICE') + 11):]
+text_items = text_items[:(text_items.find('Total USD'))]
+
+lines = text_items.splitlines()
+non_empty_lines = [line for line in lines if line.strip() != ""]
+
+items_list = set()
+text_items = ''
+for line in non_empty_lines:
+    if line[0].isdigit() and line[1] == ' ':
+        items_list.add(text_items)
+        text_items = line[2:]
+    else:
+        text_items += line
+
+items_list.add(text_items)
+print(items_list)
+
+# processing 1st condition
+items_new = list(items_list).copy()
+for i, item in enumerate(items_list):
+    if 'USD' not in item and i == 0:
+        items_new[i+1] = item + items_new[i+1]
+        items_new.remove(item)
+    elif 'USD' not in item and i != 0:
+        items_new[i - 1] = item + items_new[i - 1]
+        items_new.remove(item)
+
+print(items_new)
